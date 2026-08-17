e0873dec825429612f2d4837698215f6dda19183
LIU Youming <youming.liu@LSG.STEE.SG>
2026-08-17 15:07:45 +0800

    commit

--- air_raid_password_mysql.sql
+++ air_raid_password_mysql.sql
@@ -1,4 +1,5 @@
 -- MySQL table for the single air-raid confirmation password.
+
 CREATE TABLE IF NOT EXISTS PA_AIR_RAID_PASSWORD (
     PASSWORD_ID TINYINT UNSIGNED NOT NULL,
     PASSWORD_MD5 CHAR(32) NOT NULL,
@@ -7,7 +8,4 @@ CREATE TABLE IF NOT EXISTS PA_AIR_RAID_PASSWORD (
     CONSTRAINT CK_PA_AIR_RAID_PASSWORD_ID CHECK (PASSWORD_ID = 1)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- Initialise exactly one password before enabling this function. Replace the
--- placeholder with a controlled initial password, then execute this statement:
--- INSERT INTO PA_AIR_RAID_PASSWORD (PASSWORD_ID, PASSWORD_MD5, UPDATED_AT)
--- VALUES (1, MD5('<initial-password>'), NOW(3));
+INSERT INTO PA_AIR_RAID_PASSWORD (PASSWORD_ID, PASSWORD_MD5, UPDATED_AT) VALUES (1, MD5('<initial-password>'), NOW(3));
